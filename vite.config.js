fcc0c958c178a4c69c9db4f5b502b3006d4ef8eb
Elanur Bulbul <126582672+elanurbulbul@users.noreply.github.com>
2024-03-05 10:17:10 +0300

    changeViteConfic.js for.app

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 const path = require('path')
 
+
 export default defineConfig({
   root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    },
-  },
   server: {
     port: 8080,
     hot: true,
